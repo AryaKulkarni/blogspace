7cd7b65495c052c182fede2853c82579d0e5613f
Arya Kulkarni <kulkarni.arya1209@gmail.com>
2023-04-10 13:43:56 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import './styles/output.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='w-full flex justify-center'>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
